c5dfc00d78195b49a49ab4abd8529836dbf40e23
ogladfelter <ogladfelter@gmail.com>
2022-07-13 00:40:52 -0500

    separate these functions to avoid a bug

--- js/heatmap.js
+++ js/heatmap.js
@@ -152,6 +152,8 @@ function drawHeatmap() {
     
         // pass this data onto superlatives() function, which will find the top good, evil, lawful, chaotic characters
         superlatives(data);
+        drawGoodEvilBeeswarm(data);
+        drawLawfulChaoticBeeswarm(data);
 
         // todo: add a function which will measure agreement/disagreement/variation among user submissions
     });
@@ -205,7 +207,7 @@ function updateHeatmap(id) {
 
 drawHeatmap();
 
-function superlatives(data) {
+function drawGoodEvilBeeswarm(data) {
     // group user submitted data by character name and find each character's average coordinate scores
     const dict = d3.nest()
         .key(function(d) { return d.id; })
@@ -216,8 +218,8 @@ function superlatives(data) {
         })
         .entries(data);
 
-    // Create characters array
-    var characters = Object.keys(dict).map(function(key) {
+     // Create characters array
+     var characters = Object.keys(dict).map(function(key) {
         return [key, dict[key]];
     });
     
@@ -233,21 +235,8 @@ function superlatives(data) {
     });
     const goodCharacters = characters.slice(0, 5);
 
-    // 5 most chaotic characters
-    characters.sort(function(first, second) {
-        return second[1].value.lawfulChaotic - first[1].value.lawfulChaotic;
-    });
-    const mostChaotic = characters.slice(0, 5);
-
-    // 5 most lawful characters
-    characters.sort(function(first, second) {
-        return first[1].value.lawfulChaotic - second[1].value.lawfulChaotic;
-    });
-    const lawfulCharacters = characters.slice(0, 5);
-
     // combine the most good and most evil characters. Use reverse to make sure the 'most' characters are on top.
     const goodEvilData = goodCharacters.reverse().concat(evilCharacters.reverse());
-    const lawfulData = lawfulCharacters.reverse().concat(mostChaotic.reverse());
 
     // set the dimensions and margins of the graph
     var margin = {top: 0, right: 70, bottom: 30, left: 70},
@@ -263,7 +252,105 @@ function superlatives(data) {
     .append("g")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
-    var svg2 = d3.select("#lawfulBeeswarm")
+
+     // defs and pictures
+     var  defs = svg.append('svg:defs');
+     var config = {
+       "avatar_size": 60 // define the size of the circle radius
+     }
+     goodEvilData.forEach(function(d) {
+       defs.append("svg:pattern")
+         .attr("id", "grump_avatar" + d[1].key)
+         .attr("width", "100%") 
+         .attr("height", "100%")
+         .attr("patternUnits", "objectBoundingBox")
+         .append("svg:image")
+         .attr("xlink:href", "img/" + d[0] + ".png")
+         .attr("width", config.avatar_size)
+         .attr("height", config.avatar_size)
+         .attr("preserveAspectRatio", "none")
+         .attr("x", 0)
+         .attr("y", 0);   
+     });
+
+      // Add a X axis
+    var x = d3.scaleLinear()
+        .domain([-1000, 1000])
+        .range([0, width]);
+    svg
+        .append("g")
+        .style('font-size', '16px')
+        .attr("transform", "translate(0," + (height + 5) + ")")
+        .call(d3.axisBottom(x).ticks(2).tickFormat(function (d, i) { return ['Good','Neutral','Evil'][i]}))
+        .selectAll("text");
+
+     // Use d3-force algorithm to find a position for each entity
+     var simulation = d3.forceSimulation(goodEvilData)
+        .force("x", d3.forceX(function(d) { return x(d[1].value.goodEvil); }).strength(5))
+        .force("y", d3.forceY(height / 2))
+        .force("collide", d3.forceCollide(config.avatar_size / 2 * .75))
+        .stop();
+    for (var i = 0; i < 300; ++i) simulation.tick();
+
+    // prepare data
+    var cell = svg.append("g")
+    .selectAll("g")
+    .data(d3.voronoi()
+        .extent([[-margin.left, -margin.top], [width + margin.right, height + margin.top]])
+        .x(function(d) { return d.x; })
+        .y(function(d) { return d.y; })
+        .polygons(goodEvilData)
+    )
+    .enter()
+    .append("g");
+
+      // Finally append the circles
+      cell.append("circle")
+      .attr("r", config.avatar_size / 2)
+      .attr("class", "summaryDot")
+      .attr("cx", function(d) { return d.data.x; })
+      .attr("cy", function(d) { return d.data.y; })
+      .style("fill", function(d) { return "url(#grump_avatar" + d.data[1].key})
+      .on('mouseover', function() { d3.select(this).raise(); });
+}
+
+function drawLawfulChaoticBeeswarm(data) {
+    // group user submitted data by character name and find each character's average coordinate scores
+    const dict = d3.nest()
+        .key(function(d) { return d.id; })
+        .rollup(function(v) { return {
+                'lawfulChaotic': d3.mean(v, function(d) { return d.x_coord; }),
+                'goodEvil': d3.mean(v, function(d) { return d.y_coord; }), 
+            }
+        })
+        .entries(data);
+
+    // Create characters array
+    var characters = Object.keys(dict).map(function(key) {
+        return [key, dict[key]];
+    });
+
+    // 5 most chaotic characters
+    characters.sort(function(first, second) {
+        return second[1].value.lawfulChaotic - first[1].value.lawfulChaotic;
+    });
+    const mostChaotic = characters.slice(0, 5);
+
+    // 5 most lawful characters
+    characters.sort(function(first, second) {
+        return first[1].value.lawfulChaotic - second[1].value.lawfulChaotic;
+    });
+    const lawfulCharacters = characters.slice(0, 5);
+
+    // combine the most lawful and most chaotic characters. Use reverse to make sure the 'most' characters are on top.
+    const lawfulData = lawfulCharacters.reverse().concat(mostChaotic.reverse());
+
+    // set the dimensions and margins of the graph
+    var margin = {top: 0, right: 70, bottom: 30, left: 70},
+    width = 700 - margin.left - margin.right,
+    height = 250 - margin.top - margin.bottom;
+
+    var svg = d3.select("#lawfulBeeswarm")
         .append("svg")
         .attr("class", "summary")
         .attr("width", width + margin.left + margin.right)
@@ -277,20 +364,6 @@ function superlatives(data) {
     var config = {
       "avatar_size": 60 // define the size of the circle radius
     }
-    goodEvilData.forEach(function(d) {
-      defs.append("svg:pattern")
-        .attr("id", "grump_avatar" + d[1].key)
-        .attr("width", "100%") 
-        .attr("height", "100%")
-        .attr("patternUnits", "objectBoundingBox")
-        .append("svg:image")
-        .attr("xlink:href", "img/" + d[0] + ".png")
-        .attr("width", config.avatar_size)
-        .attr("height", config.avatar_size)
-        .attr("preserveAspectRatio", "none")
-        .attr("x", 0)
-        .attr("y", 0);   
-    });
     lawfulData.forEach(function(d) {
         defs.append("svg:pattern")
           .attr("id", "grump_avatar" + d[1].key)
@@ -314,41 +387,17 @@ function superlatives(data) {
         .append("g")
         .style('font-size', '16px')
         .attr("transform", "translate(0," + (height + 5) + ")")
-        .call(d3.axisBottom(x).ticks(2).tickFormat(function (d, i) { return ['Good','Neutral','Evil'][i]}))
-        .selectAll("text");
-    svg2
-        .append("g")
-        .style('font-size', '16px')
-        .attr("transform", "translate(0," + (height + 5) + ")")
         .call(d3.axisBottom(x).ticks(2).tickFormat(function (d, i) { return ['Lawful','Neutral','Chaotic'][i]}))
         .selectAll("text");
 
-    // Use d3-force algorithm to find a position for each entity
-    var simulation = d3.forceSimulation(goodEvilData)
-        .force("x", d3.forceX(function(d) { return x(d[1].value.goodEvil); }).strength(5))
-        .force("y", d3.forceY(height / 2))
-        .force("collide", d3.forceCollide(config.avatar_size / 2 * .75))
-        .stop();
-    for (var i = 0; i < 300; ++i) simulation.tick();
-    var simulation2 = d3.forceSimulation(lawfulData)
+    var simulation = d3.forceSimulation(lawfulData)
         .force("x", d3.forceX(function(d) { return x(d[1].value.lawfulChaotic); }).strength(5))
         .force("y", d3.forceY(height / 2))
         .force("collide", d3.forceCollide(config.avatar_size / 2 * .75))
         .stop();
-    for (var i = 0; i < 300; ++i) simulation2.tick();
+    for (var i = 0; i < 300; ++i) simulation.tick();
 
-    // prepare data
     var cell = svg.append("g")
-    .selectAll("g")
-    .data(d3.voronoi()
-        .extent([[-margin.left, -margin.top], [width + margin.right, height + margin.top]])
-        .x(function(d) { return d.x; })
-        .y(function(d) { return d.y; })
-        .polygons(goodEvilData)
-    )
-    .enter()
-    .append("g");
-    var cell2 = svg2.append("g")
     .selectAll("g")
     .data(d3.voronoi()
         .extent([[-margin.left, -margin.top], [width + margin.right, height + margin.top]])
@@ -359,7 +408,6 @@ function superlatives(data) {
     .enter()
     .append("g");
 
-    // Finally append the circles
     cell.append("circle")
         .attr("r", config.avatar_size / 2)
         .attr("class", "summaryDot")
@@ -367,13 +415,23 @@ function superlatives(data) {
         .attr("cy", function(d) { return d.data.y; })
         .style("fill", function(d) { return "url(#grump_avatar" + d.data[1].key})
         .on('mouseover', function() { d3.select(this).raise(); });
-    cell2.append("circle")
-        .attr("r", config.avatar_size / 2)
-        .attr("class", "summaryDot")
-        .attr("cx", function(d) { return d.data.x; })
-        .attr("cy", function(d) { return d.data.y; })
-        .style("fill", function(d) { return "url(#grump_avatar" + d.data[1].key})
-        .on('mouseover', function() { d3.select(this).raise(); });
+}
+
+function superlatives(data) {
+    // group user submitted data by character name and find each character's average coordinate scores
+    const dict = d3.nest()
+        .key(function(d) { return d.id; })
+        .rollup(function(v) { return {
+                'lawfulChaotic': d3.mean(v, function(d) { return d.x_coord; }),
+                'goodEvil': d3.mean(v, function(d) { return d.y_coord; }), 
+            }
+        })
+        .entries(data);
+
+    // Create characters array
+    var characters = Object.keys(dict).map(function(key) {
+        return [key, dict[key]];
+    });
 
     // using distance formula to determine most lawful-good, chaotic-good, lawful-evil, chaotic-evil, and true neutral
     // most neutral
@@ -465,7 +523,6 @@ function superlatives(data) {
         return first[1] - second[1];
     });
     document.getElementById("mostLawfulEvil").src = "img/" + objectKeys[0][0] + ".png"; // most lawful evil
-
 }
 
 function variance(data) {
